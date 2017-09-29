a20d520bd359b978a3b5ee4003b464aacc2dbc68
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-29 17:08:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,16 @@
 #require 'pry'
 
 def key_for_min_value(name_hash)
-  smallest = 100000
-  key = nil
+  min_value = nil
+  min_key = nil
   name_hash.each do |key, value|
-    #binding.pry
-    if value < smallest
-      smallest = value
-      key = key
+    if min_value == nil
+      min_value = value
+      min_key = key
+    elsif min_value > value
+      min_value = value
+      min_key = key
     end
   end
-  key
+  min_key
 end
